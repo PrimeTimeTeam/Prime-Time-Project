a631d93cf4a023e3067d77020227f7516a2c11c7
ibes-a <i.abdi@hotmail.com>
2016-11-16 23:13:14 -0800

    Added entry box

--- GUI.py
+++ GUI.py
@@ -21,8 +21,11 @@ root = Tk()  # Creats object root that has properties for the window. Access via
 root.font = ('Verdana', '20', 'bold')   #changes the font for ALL text belonging to root
 root.title('PrimeTime Software')
 Ybutton = Button(root, text="Prime", command=Pfunction(), fg='blue',bg='green',cursor= 'arrow' ).pack()  # currently packed just to populate the message box
-Nbutton = Button(root, text="Not-Prime", command=Nfunction(), fg='black', bg='red',cursor= 'arrow').pack()  # need to link to functions
-
+Nbutton = Button(root, text="Not-Prime", command=Nfunction(), fg='black', bg='red',cursor= 'arrow').pack()  # need to link to fun
+timer1 = Label(root, text = "placeholder text, ref FB timer")
+timer1.pack()
+timer2 = Label(root, text = "placeholder text, ref SB timer")
+timer2.pack()
 number = Entry(root)
 number.pack()
 number.insert(0,'value for prime')
