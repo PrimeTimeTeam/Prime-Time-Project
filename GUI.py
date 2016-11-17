9af2882cf156cf515c9830a0ccf7cbad0aaa9acb
ibes-a <i.abdi@hotmail.com>
2016-11-16 20:23:32 -0800

    Added entry box

--- GUI.py
+++ GUI.py
@@ -1,29 +1,36 @@
 # Project Name: Prime-Time-Project
 # File: GUI.py
 # NOTE: this is our graphical user interface
-import index
+#import index
 from Tkinter import *  # Importing the Tkinter (tool box) library
                         #might need be "tkinter"
+def Pfunction():
+    pass  # some code here hitting yes, pass just means no code is running here
 
+    return
+
+
+def Nfunction():
+    pass  # just pass this function for now
+    return
+
+#------------
 root = Tk()  # Creats object root that has properties for the window. Access via .instr
 
 # configuration portion
 root.font = ('Verdana', '20', 'bold')   #changes the font for ALL text belonging to root
+root.title('PrimeTime Software')
+Ybutton = Button(root, text="Prime", command=Pfunction(), fg='blue',bg='green',cursor= 'arrow' ).pack()  # currently packed just to populate the message box
+Nbutton = Button(root, text="Not-Prime", command=Nfunction(), fg='black', bg='red',cursor= 'arrow').pack()  # need to link to functions
 
-def pfunction():
-    pass  # some code here hitting yes, pass just means no code is running here
-    return
+number = Entry(root)
+number.pack()
+number.insert(0,'value for prime')
 
-
-def npfunction():
-    pass  # just pass this function for now
-    return
-
-
-Ybutton = Button(root, text="Prime", command=pfunction(), fg='blue', bg='green',).pack()            #currently packed just to populate the message box
-Nbutton = Button(root, text="Not-Prime", command=npfunction(), fg='black', bg='red').pack()         #need to link to functions
-                                                                                                    #above for functionallity
+#above for functionallity
 
 root.mainloop()  # Execute the main event handler
+
+
 
 
