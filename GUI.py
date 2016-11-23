6f2dca092bc46999e8f9f4f4710f7e66f8ab4851
ibes-a <i.abdi@hotmail.com>
2016-11-22 23:13:52 -0800

    trying to add a login class. having trouble bringing up window

--- GUI.py
+++ GUI.py
@@ -4,6 +4,26 @@
 #import index
 from Tkinter import *  # Importing the Tkinter (tool box) library
                         #might need be "tkinter"
+
+class LoginWindow(Frame):
+    def _init_(self,master):
+        self._init_(self,master)
+        self.title('Login Window')
+        self.label1= Label(self, text= "Username")
+        self.label2= Label(self, text= "Password")
+        self.enter1 = Entry(self)
+        self.enter2 = Entry(self, show= "*")            #this maskes password on entry 2
+        #grid config row, col, stick
+
+        self.label1.grid(row=0, sticky=W)
+        self.label2.grid(row=1,sticky=W)
+        self.enter1.grid(row=0,column=2)
+        self.enter2.grid(row=1,column =2)
+
+
+
+        self.pack()
+
 def Pfunction():
     pass  # some code here hitting yes, pass just means no code is running here
 
@@ -16,6 +36,8 @@ def Nfunction():
 
 #------------
 root = Tk()  # Creats object root that has properties for the window. Access via .instr
+lw = LoginWindow(root)
+
 
 # configuration portion
 root.font = ('Verdana', '20', 'bold')   #changes the font for ALL text belonging to root
@@ -30,10 +52,9 @@ number = Entry(root)
 number.pack()
 number.insert(0,'value for prime')
 
+root.mainloop()
 #above for functionallity
 
-root.mainloop()  # Execute the main event handler
+#root.mainloop()  # Execute the main event handler
 
-
-
-
+#login class
