66dad331cf288c4e3e3107acd58e022c57084270
i-abdi <i.abdi@hotmail.com>
2016-11-28 09:28:11 -0800

    admin window

--- GUI.py
+++ GUI.py
@@ -12,29 +12,43 @@ count = 0
 def centerWindow():
     x = (root.winfo_screenwidth() - root.winfo_reqwidth()) / 2
     y = (root.winfo_screenheight() - root.winfo_reqheight()) / 2
-    root.geometry("+%d+%d" % (x, y))
-
-def adminButton():
+    root.geometry("+%d+%d" % (x, y))                         #takes in variables for centering
+def dbConfig():
     #make new window, allow for flushing db
-
-    window = tk.Toplevel(root)
-    flushdbButton = Button(window,text="flush database")
-    secondButton = Button(window, text= " do something else") #add ,command = DB.flush()
+    windowDB = tk.Toplevel(root)
+    flushdbButton = Button(windowDB, text="flush database")
+    secondButton = Button(windowDB, text=" do something else") #add ,command = DB.flush()
     flushdbButton.pack()
     secondButton.pack()
-    pass
+
+def loginButton():
+    windowLog = tk.Toplevel(root)
+    usernameEntry = Entry(windowLog)
+    usernameLabel = Label(windowLog, text= "Username:")
+    passwordEntry = Entry(windowLog, show= "*")
+    passwordLabel = Label(windowLog, text= "Password:")
+    loginButton = Button(windowLog, text="Login")
+    usernameEntry.grid(row=0, column=1)
+    passwordEntry.grid(row=1, column=1)
+    usernameLabel.grid(row=0, column=0)
+    passwordLabel.grid(row=1, column=0)
+    loginButton.grid(row=2, column=1)
+
+
+
+
 def primeCheck(event):
-    if re.search('[a-zA-Z]', entry1.get()) or set(' [~!=@#$%^&*()_+{}":;\']+$').intersection(entry1.get()):
+    if re.search('[a-zA-Z]', entry1.get()) or set(' [~!=@#$%^`&*()_+{}":;\']+$').intersection(entry1.get()):
         tkMessageBox.showwarning("INVALID ENTRY", "Please enter only digits")
         return
     if brain(int(entry1.get())):
         Pbutton.config(bg='Green')
         root.after(1000, lambda: Pbutton.config(bg='grey'))
-        pass
+
     else:
         NPbutton.config(bg='Red')
         root.after(1000, lambda: NPbutton.config(bg='grey'))
-        pass
+
 
 def sbTime():
      pass   # placeholder string
@@ -43,6 +57,9 @@ def sbTime():
 def fbTime():
     return "test"  # placeholder string
 
+
+
+#main portion of GUI
 root = Tk()  # Creates object root that has properties for the window. Access via .instr
 centerWindow()
 # configuration portion
@@ -55,7 +72,8 @@ entry1 = Entry(root)
 mesgLabel = Label(root, text="Enter Value to Check: ", font="Times 12")
 Pbutton = Label(root, text="Prime", fg='Black', bg='grey', font="Times 20")
 NPbutton = Label(root, text="Not-Prime", fg='black', bg='grey', font="Times 20")
-adminbutton = Button(root, command=adminButton, text= "Enter Admin Mode", fg='black', bg='grey')
+loginWindow = Button(root, command=loginButton, text= "Enter Admin Mode", fg='black', bg='grey')
+
 
 
 #MUST SEPERATE CONFIG. this allows for 'plabel' and 'pbutton' to still point at the button object.
@@ -69,7 +87,7 @@ mesgLabel.grid(row=2,column=0)
 Pbutton.grid(row=0,  column=0)
 NPbutton.grid(row=0, column=1)
 entry1.bind('<Return>', primeCheck)
-adminbutton.grid(row=2, column=2)
+loginWindow.grid(row=2, column=2)
 
 
 root.mainloop()  # Execute the main event handler
